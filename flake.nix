f0c3842c1c676d6b6b32b870f9138e50e9c8236d
Hatim Thayyil <hatim@thayyil.net>
2025-04-10 22:23:54 +0100

    add more flake inputs, prep for modularised flake

--- flake.nix
+++ flake.nix
@@ -2,22 +2,74 @@
   description = "Nix configuration flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    #
+    # ========== Official NixOS
+    #
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Pinned versions for critical applications
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/master";
+      #url = "github:nix-community/home-manager/release-24.11";
+
+      # To be kept up to date with nixpkgs
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # Hardware
+    hardware.url = "github:nixos/nixos-hardware/master";
+    # Nix User Repository
     nur = {
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    #
+    # ========== Utilities
+    #
+    # Declarative partitioning and formatting
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # Impermanance
+    impermanence = {
+      url = "github:nix-community/impermanence";
+    };
+    # Secrets management.
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # Nix based neovim
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+      #url = "github:nix-community/nixvim";
+      #inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    # Pre-commit
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # Emacs
     emacs-overlay = {
       url = "github:nix-community/emacs-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nur, emacs-overlay, ... }:
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    nur,
+    emacs-overlay,
+    ...
+  }@inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
