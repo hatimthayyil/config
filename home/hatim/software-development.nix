6d6e5c612e1fbb07e4e0bf7dce5776c20712f6d0
Hatim Thayyil <hatim@thayyil.net>
2025-04-18 13:15:21 +0100

    add github tools

--- home/hatim/software-development.nix
+++ home/hatim/software-development.nix
@@ -5,6 +5,18 @@
   programs = {
     direnv.enable = true;
     jq.enable = true; # JSON processor
+
+    gh = {
+      enable = true;
+      extensions = [
+        pkgs.gh-s # search repos
+        pkgs.gh-i # search issues
+        pkgs.gh-f # fzf
+        pkgs.gh-poi # clean up local branches
+      ];
+    };
+    # GitHub dashboard (can be customised)
+    gh-dash.enable = true;
   };
   home.packages = [
     # Dev
@@ -13,5 +25,9 @@
     pkgs.curl
     pkgs.curlie # frontend for curl
     pkgs.shellcheck
+
+    # Remote repo management
+    pkgs.ghorg # clone entire org/user repos
+    pkgs.github-backup # backup github
   ];
 }
