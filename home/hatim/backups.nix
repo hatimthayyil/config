c09b40fea39d0b82c25cf4a2e62bea1810e22e86
Hatim Thayyil <hatim@thayyil.net>
2025-04-18 09:20:27 +0100

    remove borgmatic for now

--- home/hatim/backups.nix
+++ home/hatim/backups.nix
@@ -2,8 +2,8 @@
   ...
 }: {
   # TODO Setup a backup routine. NixOS has services for the restic backup tool.
-  programs.borgmatic = {
-    enable = true;
-  };
+  #programs.borgmatic = {
+  #  enable = true;
+  #};
   #services.borgmatic.enable = true;
 }
