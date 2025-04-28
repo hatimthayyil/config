abcab12f8dcb666377500baa325f990c099717bd
Hatim Thayyil <hatim@thayyil.net>
2025-04-28 07:47:23 +0100

    home: add zotero

--- home/hatim/research-tools.nix
+++ home/hatim/research-tools.nix
@@ -3,6 +3,7 @@
   ...
 }: {
   home.packages = [
+    pkgs.zotero # reference management
     pkgs.monolith # archive web-page
     pkgs.xdot # graph viewer
     pkgs.gephi # Visualise and manipulate large graphs
