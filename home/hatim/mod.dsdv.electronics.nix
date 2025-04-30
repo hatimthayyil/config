c70ccba72b6eb4c8f5514c72d33dfdbfa50a41ee
Hatim Thayyil <hatim@thayyil.net>
2025-04-30 19:28:28 +0100

    fyr: microcap

--- home/hatim/mod.dsdv.electronics.nix
+++ home/hatim/mod.dsdv.electronics.nix
@@ -2,6 +2,8 @@
   pkgs,
   ...
 }: {
+  # TODO Wine-compatible Microcap is provided by
+  # https://github.com/emmanuelrosa/erosanix
   home.packages = [
     pkgs.unstable.kicad # unstable is broken
   ];
