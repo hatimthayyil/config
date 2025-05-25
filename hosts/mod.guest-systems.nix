c943aa53f3008b6b4df624bb14f2864f2521faa4
Hatim Thayyil <hatim@thayyil.net>
2025-05-25 16:38:53 +0100

    os: macos-ventura, add settings, increase ram, cpu, remove autostart

--- hosts/mod.guest-systems.nix
+++ hosts/mod.guest-systems.nix
@@ -5,6 +5,10 @@
     enable = true;
     openFirewall = true;
     vncListenAddr = "0.0.0.0";
+    cores = 3;
+    threads = 6;
+    mem = "18G";
+    autoStart = false;
   };
 
   environment.systemPackages = [
