123a06ea500b1806d9d038e3bbf65e9927c8e4c1
Hatim Thayyil <hatim@thayyil.net>
2025-06-19 23:50:03 +0100

    home: qnotero (zotero search)

--- home/hatim/mod.tools.research.nix
+++ home/hatim/mod.tools.research.nix
@@ -5,6 +5,7 @@
 {
   home.packages = [
     pkgs.zotero # reference management
+    pkgs.qnotero # quick access to zotero references
     pkgs.monolith # archive web-page
     pkgs.xdot # graph viewer
     pkgs.gephi # Visualise and manipulate large graphs
