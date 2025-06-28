545ca7867d281bacf440bb3d3303a7b7e509bcb3
Hatim Thayyil <hatim@thayyil.net>
2025-06-28 11:19:49 +0100

    home: add suckit - save whole websites

--- home/hatim/mod.tools.research.nix
+++ home/hatim/mod.tools.research.nix
@@ -7,6 +7,7 @@
     pkgs.zotero # reference management
     pkgs.qnotero # quick access to zotero references
     pkgs.monolith # archive web-page
+    pkgs.suckit # archive entirity of a website
     pkgs.xdot # graph viewer
     pkgs.gephi # Visualise and manipulate large graphs
     pkgs.datasette
