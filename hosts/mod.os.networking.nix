cf65a012a56ce714a98c0f2955147a80c1efab23
Hatim Thayyil <hatim@thayyil.net>
2025-08-31 00:50:10 +0100

    add opensnitch rules

--- hosts/mod.os.networking.nix
+++ hosts/mod.os.networking.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, lib, ... }:
 {
   programs.wireshark.enable = true;
 
@@ -6,5 +6,69 @@
   # home-manager.
   services.opensnitch = {
     enable = true;
+    rules = {
+      # Programs
+      systemd-timesyncd = {
+        name = "systemd-timesyncd";
+        enabled = true;
+        action = "allow";
+        duration = "always";
+        operator = {
+          type ="simple";
+          sensitive = false;
+          operand = "process.path";
+          data = "${lib.getBin pkgs.systemd}/lib/systemd/systemd-timesyncd";
+        };
+      };
+      nsncd = {
+        name = "nsncd";
+        enabled = true;
+        action = "allow";
+        duration = "always";
+        operator = {
+          type ="simple";
+          sensitive = false;
+          operand = "process.path";
+          data = "${lib.getBin pkgs.nsncd}/bin/nsncd";
+        };
+      };
+      zellij = {
+        name = "zellij";
+        enabled = true;
+        action = "allow";
+        duration = "always";
+        operator = {
+          type ="simple";
+          sensitive = false;
+          operand = "process.path";
+          data = "${lib.getBin pkgs.zellij}/bin/zellij";
+        };
+      };
+      zotero = {
+        name = "zotero";
+        enabled = true;
+        action = "allow";
+        duration = "always";
+        operator = {
+          type ="simple";
+          sensitive = false;
+          operand = "process.path";
+          data = "${lib.getBin pkgs.zotero}/bin/zotero";
+        };
+      };
+
+      # Hosts
+      crates-io = {
+        name = "crates-io";
+        enabled = true;
+        action = "allow";
+        duration = "always";
+        operator = {
+          type ="regexp";
+          operand = "dest.host";
+          data = "^(|.*\\.)crates\\.io$";
+        };
+      };
+    };
   };
 }
