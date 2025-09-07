02493528965c4fa2d105e7b8ce991d47f74b9926
Hatim Thayyil <hatim@thayyil.net>
2025-09-07 11:56:19 +0100

    home: add neo4j

--- home/hatim/mod.tools.research.nix
+++ home/hatim/mod.tools.research.nix
@@ -10,6 +10,7 @@
     pkgs.suckit # archive entirity of a website
     pkgs.xdot # graph viewer
     pkgs.gephi # Visualise and manipulate large graphs
+    pkgs.neo4j # native graph db
     pkgs.datasette
     pkgs.wiki-tui # Wikipedia in CLI
   ];
