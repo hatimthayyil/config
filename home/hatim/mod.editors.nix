03f8f272d969582e3ddc403373ea548ed7567791
Hatim Thayyil <hatim@thayyil.net>
2026-01-17 09:30:07 +0000

    home: update vscode extns to 1.107.1

--- home/hatim/mod.editors.nix
+++ home/hatim/mod.editors.nix
@@ -53,7 +53,7 @@
     enable = true;
     package = pkgs.unstable.vscode;
     profiles.default = {
-      extensions = pkgs.nix4vscode.forVscodeVersion "1.106.3" [
+      extensions = pkgs.nix4vscode.forVscodeVersion "1.107.1" [
         "github.copilot"
 
         # Development Tools
