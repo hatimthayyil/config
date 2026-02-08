3dfb9f5d44e2ee621124194796e5395b659d6395
Hatim Thayyil <hatim@thayyil.net>
2026-02-08 16:33:53 +0000

    home: add devenv

---
+++ devenv.nix
@@ -0,0 +1,24 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  packages = [ pkgs.git ];
+
+  # languages.nix.enable = true;
+
+  treefmt = {
+    enable = true;
+    config.programs = {
+      nixfmt.enable = true;
+    };
+  };
+
+  git-hooks.hooks = {
+    shellcheck.enable = true;
+  };
+}
