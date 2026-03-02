f143995de28caf4c28f29360557770f2af346b64
Hatim Thayyil <hatim@thayyil.net>
2026-03-02 21:26:20 +0000

    fix(home): remove claude

--- home/hatim/mod.language-machine.nix
+++ home/hatim/mod.language-machine.nix
@@ -20,7 +20,7 @@
     #LEAN pkgs.jan
     pkgs.aider-chat
     #pkgs.aider-chat-full # FIXME broken on unstable
-    pkgs.unstable.claude-code # Coder agent from Anthropic
+    # pkgs.unstable.claude-code # Coder agent from Anthropic # install from npm for now
     pkgs.unstable.codex # Coder agent from OpenAI
     pkgs.unstable.opencode # AI coding agent
     pkgs.unstable.opencode-desktop # GUI for opencode
