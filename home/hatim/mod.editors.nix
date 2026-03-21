4e8c514918684fa251febedfcbf4eb190796b5bb
Hatim Thayyil <hatim@thayyil.net>
2026-03-21 21:13:22 +0000

    home: use FHS Zed editor

--- home/hatim/mod.editors.nix
+++ home/hatim/mod.editors.nix
@@ -126,7 +126,7 @@
   # Zed
   programs.zed-editor = {
     enable = true;
-    package = pkgs.unstable.zed-editor;
+    package = pkgs.unstable.zed-editor-fhs;
     extensions = [
       "codebook"
 
