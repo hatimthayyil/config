05fd288ac4289c4573758bbf24d2711b496529bd
Hatim Thayyil <hatim@thayyil.net>
2026-05-30 10:01:38 +0100

    chore: fmt

--- modules/dev-mechanical.nix
+++ modules/dev-mechanical.nix
@@ -6,12 +6,11 @@ let
   inherit (config) owner;
 in
 {
-  flake.modules.nixos.dev-mechanical =
-    _: {
-      home-manager.users.${owner.username} = {
-        home.packages = [
-          # pkgs.freecad
-        ];
-      };
+  flake.modules.nixos.dev-mechanical = _: {
+    home-manager.users.${owner.username} = {
+      home.packages = [
+        # pkgs.freecad
+      ];
     };
+  };
 }
